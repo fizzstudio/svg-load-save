6363d4e042565f34aa9b100c08fa06aca1239db1
Doug Schepers <doug@fizz.studio>
2020-07-25 21:04:55 -0400

    modularized, automated file list

--- modularized/js/svg-edit.js
+++ modularized/js/svg-edit.js
@@ -1,5 +1,5 @@
-/* Adds a stroke to any SVG element that's been clicked on */
-function add_stroke(event) {
+export function add_stroke(event) {
+  /* Adds a stroke to any SVG element that's been clicked on */
   const target_el = event.target;
   target_el.setAttribute("stroke", "gray");
   target_el.setAttribute("stroke-width", "3");
